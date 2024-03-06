e0cd276310164d992b6e00b013c9acccc0f50d07
kiran <verti_kiran_vishwakarma@golfdistrict.com>
2024-03-06 20:25:48 +0530

    First Test

---
+++ tests/first-test.spec.ts
@@ -0,0 +1,25 @@
+import {test} from '@playwright/test'
+
+
+
+test.describe('test suite 1', () => {
+    
+    test('the first test', () => {
+
+
+
+    })
+
+    test('the second  test', () => {
+
+
+
+    })
+
+    test('the third test', () => {
+
+
+
+    })
+
+})
